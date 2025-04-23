9838524845fe2f0e2da4fa4e72e817d2e95d4248
Shishir707 <mirshr8@gmail.com>
2025-04-23 22:11:26 +0600

    added bus list

--- script.js
+++ script.js
@@ -7,16 +7,66 @@ function search(){
     
 
     if (from === to){
-        document.getElementById("eror").innerText = "Please Chosse Different Terminal";
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Please choose different terminals.",
+        });
+        return;
     }
 
     else if (date===""){
-        document.getElementById("eror").innerText ="Please Pick Your Journey Date"
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Pick your journey date.",
+        });
+        return;
+    }
+
+    else if (from == "Dhaka" && to == "Cox's Bazar"){
+        const oldContent = document.getElementById("busList");
+            oldContent.textContent = "";
+
+            const greenLine = document.createElement("div");
+            greenLine.classList.add("innerStyle");
+            greenLine.innerHTML = `<img src="green.jpeg" alt="Green Line Bus">
+                                <div>
+                                <h2>Green Line</h2>
+                                Dhaka -----------------> Cox's Bazar
+                                <p>08:00 PM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;04:00 AM</p><br>
+                                <button onClick="green()" id="btn" >Book Now</button>
+                                </div>`;
+            oldContent.appendChild(greenLine);
+
+            const srTravel = document.createElement("div")
+            srTravel.classList.add("innerStyle");
+            srTravel.innerHTML = `<img src="green.jpeg">
+                                <br>
+                                <div>
+                                <h2>SR Travels</h2>
+                                Dhaka -----------------> Cox's Bazar
+                                <p>10:00 PM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;06:00 AM</p><br>
+                                <button onClick="sr()" id="btn" >Book Now</button>
+                                </div>`;
+            oldContent.appendChild(srTravel);
+    
+
+    }
+
+    else if (["Cox's Bazar","Khulna","Rajshahi"].includes(from) || ["Rajshahi","Khulna"].includes(to)) {
+        Swal.fire({
+            icon: "error",
+            title: "Sorry!!",
+            text: "No bus availabe for this route.",
+        });
+        return;
     }
 
     else{
         document.getElementById("eror").innerText = "";
     }
+    
 
     
 }
