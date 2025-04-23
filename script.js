a0ab9aceaa3efe25c61bad039bfd94e74c9b9264
Shishir707 <mirshr8@gmail.com>
2025-04-23 23:12:10 +0600

    add seat layout

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function search(){
                                 <h2>SR Travels</h2>
                                 Dhaka -----------------> Cox's Bazar
                                 <p>10:00 PM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;06:00 AM</p><br>
-                                <button onClick="sr()" id="btn" >Book Now</button>
+                                <button onClick="srTravel()" id="btn" >Book Now</button>
                                 </div>`;
             oldContent.appendChild(srTravel);
     
@@ -59,6 +59,7 @@ function search(){
             icon: "error",
             title: "Sorry!!",
             text: "No bus availabe for this route.",
+            footer:"Only Dhaka Cox's Bazar availabe now."
         });
         return;
     }
@@ -66,7 +67,27 @@ function search(){
     else{
         document.getElementById("eror").innerText = "";
     }
-    
-
-    
 }
+
+function green(){
+    const layout = document.getElementById("seatLayout").value ;
+    layout.innerHTML="";
+
+    let seatHTML = `<h3>Select Your Seat<h3> <div id="busLayout">`
+
+    const row =  ["A","B","C","D"];
+
+    for (let r =0; r<row.length; r++){
+        for (let c =0; c<8; c++){
+            const seatID = row[r]+c;
+
+            seatHTML +=`<button class="seat" id=${seatID} onClick="selectSeat(${seatID})">${seatID}</button>`
+
+        }
+
+        seatHTML +="<br><br>"
+    }
+    seatHTML+= `</div> <br> <button onClick="confirm()" id="btn">Confirm Purchase</button>`
+}
+
+
